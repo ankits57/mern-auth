ba19ccd826f289c44588f6f592770c503a2fe63c
Ankit Saurabh <ankitsaurabh57@gmail.com>
2024-07-24 21:32:14 +0530

    create header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,12 +8,14 @@ import About from "./pages/About";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
+import Header from "./components/Header";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
